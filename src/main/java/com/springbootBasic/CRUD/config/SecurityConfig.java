75aaea38d96a35c78eb37bae6d44fbd6177cff7f
asmitgupta1 <guptaasmit1@gmail.com>
2024-08-15 00:20:10 +0530

    JWT authentication

--- src/main/java/com/springbootBasic/CRUD/config/SecurityConfig.java
+++ src/main/java/com/springbootBasic/CRUD/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +29,11 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+
     @Bean
     public AuthenticationProvider authProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
@@ -38,11 +46,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers("register","login")
+                                .permitAll()
+                                .anyRequest().authenticated());
 //        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager
+            (AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+
     }
 
 
--- src/main/java/com/springbootBasic/CRUD/controller/UserRestController.java
+++ src/main/java/com/springbootBasic/CRUD/controller/UserRestController.java
@@ -3,7 +3,11 @@ package com.springbootBasic.CRUD.controller;
 
 import com.springbootBasic.CRUD.model.User;
 import com.springbootBasic.CRUD.repo.UserRepo;
+import com.springbootBasic.CRUD.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +22,12 @@ public class UserRestController {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
 
@@ -28,10 +38,22 @@ public class UserRestController {
         return userRepo.findAll();
     }
 
-    @PostMapping("/addUser")
-    public User addUser(@RequestBody User user)
+    @PostMapping("/register")
+    public User register(@RequestBody User user)
     {
         user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(user.getUsername());
+        }
+        return "Login Failed";
     }
 }
